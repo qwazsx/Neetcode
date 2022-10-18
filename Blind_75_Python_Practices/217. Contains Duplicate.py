0cf9aec28e85976d153ab5d92bbb5f89a035ccb8
Muhammed Fatih Cambek <fatihcambek@gmail.com>
2022-10-18 23:53:30 +0300

    Add easy set/hashset problems with python

---
+++ Blind_75_Python_Practices/217. Contains Duplicate.py
@@ -0,0 +1,27 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+def containsDuplicate(nums):
+    hashset = set()
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+print(containsDuplicate([1,2,3,1]))
+print(containsDuplicate([1,2,3,4]))
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
