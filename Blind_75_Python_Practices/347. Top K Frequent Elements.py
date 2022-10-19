08430376112ae14f44f5990470e62aea205e39e1
Muhammed Fatih Cambek <fatihcambek@gmail.com>
2022-10-19 23:08:58 +0300

    More Arrays & Hashing problems

---
+++ Blind_75_Python_Practices/347. Top K Frequent Elements.py
@@ -0,0 +1,43 @@
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# k is in the range [1, the number of unique elements in the array].
+# It is guaranteed that the answer is unique.
+ 
+
+# Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {} # item : count
+        freq = [[] for i in range(len(nums) +1)] # we create a map 0 to n for storing counts -> item pair
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        for n, c in count.items(): 
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1): #start index(-1 means last), to the start of the array(index 0), increment by -1 which we go down
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+
+
+    print(topKFrequent(any, nums = [1,1,1,2,2,3], k = 2))
